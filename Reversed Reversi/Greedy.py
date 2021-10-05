c06d846c699c95567a3df3bcd8598ec6027b0f6d
MrPenguins <56572306+MrPenguins@users.noreply.github.com>
2021-10-05 13:28:40 +0800

    Create Greedy.py

---
+++ Reversed Reversi/Greedy.py
@@ -0,0 +1,93 @@
+import numpy as np
+import random
+import time
+
+COLOR_BLACK = -1
+COLOR_WHITE = 1
+COLOR_NONE = 0
+random.seed(0)
+
+
+class AI(object):
+    def __init__(self, chessboard_size, color, time_out):
+        self.chessboard_size = chessboard_size
+        self.color = color
+        self.opposite_color = -color
+        self.time_out = time_out
+        self.candidate_list = []
+        self.directions = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+
+    def go(self, chessboard):
+        self.candidate_list.clear()
+        self.candidate_list = self.possible_positions(chessboard)
+        self.candidate_list = self.greedy_approach(chessboard, self.candidate_list)
+        return self.candidate_list
+
+    # Decide the possible positions for placing a chess piece
+    def possible_positions(self, chessboard) -> list:
+        positions = []
+        for i in range(0, self.chessboard_size):
+            for j in range(0, self.chessboard_size):
+                if chessboard[i][j] == COLOR_NONE and self.check_legal(chessboard, i, j):
+                    positions.append((i, j))
+        return positions
+
+    # Decide whether the current position is legal to place a chess piece
+    def check_legal(self, chessboard, x, y) -> bool:
+        flag = False
+        for direction in self.directions:
+            possible_x = x + direction[0]
+            possible_y = y + direction[1]
+            if 0 <= possible_x < self.chessboard_size and 0 <= possible_y < self.chessboard_size and \
+                    chessboard[possible_x][possible_y] == self.opposite_color:
+                possible_x += direction[0]
+                possible_y += direction[1]
+                while 0 <= possible_x < self.chessboard_size and 0 <= possible_y < self.chessboard_size:
+                    if chessboard[possible_x][possible_y] == self.opposite_color:
+                        possible_x += direction[0]
+                        possible_y += direction[1]
+                    elif chessboard[possible_x][possible_y] == self.color:
+                        flag = True
+                        break
+                    else:
+                        break
+                if flag:
+                    break
+        return flag
+
+    # use greedy to decide where to put the chess piece
+    def greedy_approach(self, chessboard, possible_positions) -> list:
+        greedy_decision = []
+        min_points = 1000
+        for position in possible_positions:
+            points = self.greedy_count_points(chessboard, position[0], position[1])
+            if points < min_points:
+                min_points = points
+                greedy_decision.append(position)
+            else:
+                greedy_decision.insert(0, position)
+        return greedy_decision
+
+    # greedy: calculate the number of chess pieces been reversed
+    def greedy_count_points(self, chessboard, x, y) -> int:
+        points = 0
+        for direction in self.directions:
+            count = 0
+            possible_x = x + direction[0]
+            possible_y = y + direction[1]
+            if 0 <= possible_x < self.chessboard_size and 0 <= possible_y < self.chessboard_size and \
+                    chessboard[possible_x][possible_y] == self.opposite_color:
+                count = count + 1
+                possible_x += direction[0]
+                possible_y += direction[1]
+                while 0 <= possible_x < self.chessboard_size and 0 <= possible_y < self.chessboard_size:
+                    if chessboard[possible_x][possible_y] == self.opposite_color:
+                        count = count + 1
+                        possible_x += direction[0]
+                        possible_y += direction[1]
+                    elif chessboard[possible_x][possible_y] == self.color:
+                        points += count
+                        break
+                    else:
+                        break
+        return points
